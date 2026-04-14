c6dc5965e6bfb20cfb8f9f5b294f0efbcbdca608
kneeltogenius-web <kneeltogenius@gmail.com>
2026-04-14 14:04:59 +0530

    progress in frontend

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,13 @@ const PORT = process.env.PORT || 3000;
 // ✅ connect DB
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const testRoutes = require("./routes/testRoutes");
